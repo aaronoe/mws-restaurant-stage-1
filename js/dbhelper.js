09f3d5f5342328e6c048e3f2ff71584052b9174c
aaron <aaronoe97@gmail.com>
2018-05-27 13:49:48 +0200

    fetch resources from server, add manifest for pwa score

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -12,23 +12,30 @@ class DBHelper {
     return `http://localhost:${port}/data/restaurants.json`;
   }
 
+  static get SERVER_URL() {
+    const port = 1337;
+    return `http://localhost:${port}/restaurants`;
+  }
+
   /**
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
-      }
-    };
-    xhr.send();
+    fetch(this.SERVER_URL)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Network response was not ok.');
+      })
+      .then((json) => {
+        callback(null, json);
+      })
+      .catch((err) => {
+        console.log(err);
+        const errorText = (`Request failed. Returned status of ${err}`);
+        callback(errorText, null);
+      });
   }
 
   /**
@@ -150,7 +157,8 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    console.log(`Restaurant Photo: `, restaurant);
+    return (`/img/${restaurant.photograph}.jpg`);
   }
 
   /**
@@ -162,8 +170,8 @@ class DBHelper {
       title: restaurant.name,
       url: DBHelper.urlForRestaurant(restaurant),
       map: map,
-      animation: google.maps.Animation.DROP}
-    );
+      animation: google.maps.Animation.DROP
+    });
     return marker;
   }
 
